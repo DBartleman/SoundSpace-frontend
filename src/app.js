a7220cf4b0f88ed5b9eefa92656cd739e62256e7
dbartleman <drewbartleman@gmail.com>
2020-06-25 16:33:43 -0700

    front end main page skeleton and basic routes

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route {...rest} render={(props) => (
+		localStorage.getItem("SoundSpace/authentication/TOKEN")
+			? <Component {...props} />
+			: <Redirect to='/login' />
+	)} />
+)
+
+function App() {
+	return (
+		<>
+			<Switch>
+                {/* TODO: implement components below */}
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/signup" component={Signup} />
+				<PrivateRoute path="/" component={AlreadyLoggedIn} />
+			</Switch>
+		</>
+	);
+}
+
+export default App;
