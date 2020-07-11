45ac68126f8b4a77fc27eb8a6ab7cd030ef83946
dbartleman <drewbartleman@gmail.com>
2020-07-10 20:42:25 -0700

    testing new songplayer data retrieval, removed drawer

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -2,6 +2,61 @@ import React from "react";
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import "react-jinke-music-player/assets/index.css";
 
+const audioList1 = [
+  {
+    name: 'Despacito',
+    singer: 'Luis Fonsi',
+    cover:
+      'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
+    musicSrc: () => {
+      return Promise.resolve(
+        'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
+      )
+    },
+  },
+  {
+    name: 'Dorost Nemisham',
+    singer: 'Sirvan Khosravi',
+    cover:
+      'https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg',
+    musicSrc: () => {
+      return Promise.resolve(
+        'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
+      )
+    },
+  },
+]
+
+const audioList2 = [
+  {
+    name: 'Bedtime Stories',
+    singer: 'Jay Chou',
+    cover:
+      'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
+    musicSrc:
+      'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
+  },
+  {
+    name: 'Dorost Nemisham',
+    singer: 'Sirvan Khosravi',
+    cover:
+      'https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg',
+    musicSrc: () => {
+      return Promise.resolve(
+        'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
+      )
+    },
+  },
+  {
+    name: 'Despacito',
+    singer: 'Luis Fonsi',
+    cover:
+      'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
+    musicSrc: 'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3'
+  },
+]
+
+
 class SongPlayer extends React.Component{
   constructor() {
     super();
@@ -22,7 +77,7 @@ class SongPlayer extends React.Component{
         </button>
         <button onClick={() => (this.audioInstance.volume = 0.2)}>change volume</button>
         <button onClick={() => this.audioInstance.destroy()}>destroy player</button>
-        <button onClick={() => this.audio.appendAudio(1, [{ musicSrc: 'x', name: "xx" }])}>append audio</button>
+        <button onClick={() => this.audio.appendAudio(1, [{ musicSrc: '../../sample_music/single_album/01 Tank.mp3', name: "Tank!" }])}>append audio</button>
         <button onClick={this.audio.togglePlay}>toggle play</button>
         <button onClick={this.audio.clear}>clear audio lists</button>
         <button onClick={this.audio.playNext}>play next</button>
