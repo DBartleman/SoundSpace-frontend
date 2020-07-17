e3ac39358698d743a27afd0852b2bcce48e57863
dbartleman <drewbartleman@gmail.com>
2020-07-16 17:12:31 -0700

    artist page almost done, only needs background

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import Login from "./components/login";
 import Signup from "./components/signup";
 import ForgotPassword from "./components/forgotPassword";
 import Homepage from "./components/homepage";
+import ArtistPage from "./components/artistPage";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
 	<Route {...rest} render={(props) => (
@@ -20,6 +21,7 @@ function App() {
 				<Route exact path="/login" component={Login} />
 				<Route exact path="/signup" component={Signup} />
 				<Route exact path="/forgot" component={ForgotPassword} />
+				<Route exact path="/artist/:id" component={ArtistPage} />
 				<PrivateRoute path="/" component={Homepage} />
 			</Switch>
 		</>
