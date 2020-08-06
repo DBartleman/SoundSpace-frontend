5da372c2607c22ad86f415f7450d405471245799
dbartleman <drewbartleman@gmail.com>
2020-08-06 15:07:47 -0700

    last attempt at doing vibrance.js before moving on to another package

--- src/components/albumPage.js
+++ src/components/albumPage.js
@@ -4,36 +4,33 @@ import urlGenerator from "./downloader";
 
 
 class AlbumPage extends React.Component {
-  // const url = urlGenerator("music/cb/cover.jpg");
-  // getColors() {
-  //   Vibrant.from(urlGenerator("music/cb/cover.jpg")).getPalette()
-  //     .then((palette) => console.log(palette))
-  // }
-  getColors() {
-    //const pal = Vibrant.from(urlGenerator("music/cb/cover.jpg")).getPalette().then((palette) => console.log(palette))
-    //let v = new Vibrant(urlGenerator("music/cb/cover.jpg"))
-    // let v = new Vibrant("../album_art_test/03.jpg");
-    // v.getPalette((err, palette) => console.log(palette));
-
-    //   WORKS:
-    // const v = new Vibrant('../album_art_test/03.jpg');
-    // console.log(v);
-
-    Vibrant.from(urlGenerator("music/cb/cover.jpg")).getPalette(function (err, palette) { console.log(palette); });
-
-
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+  componentDidMount() { // after first render
+    debugger;
+    this.getColors(this.imageRef);
+  }
+  getColors(imageRef) {
+    debugger;
+    Vibrant.from(`${imageRef}`).getPalette(
+      (err, palette) => { 
+        debugger;
+        return palette; 
+      });
     // const v = new Vibrant(urlGenerator("music/cb/cover.jpg"));
     // console.log(v.getPalette);
   }
 
   render () {
+    debugger;
+    // this.getColors(this.imageRef);
     return (
-        <div>
-          <script>{this.getColors()}</script>
-          {/* <style>{pal}</style> */}
-          {/* <script>{console.log(pal)}</script> */}
-          {/* <img src={urlGenerator("music/cb/cover.jpg")} alt="Album Art" /> */}
-        </div>
+      <div>
+        <img src={urlGenerator("music/cb/cover.jpg")} ref={this.imageRef} alt="Album Art" />  
+        {/* {this.getColors(this.imageRef)} */}
+      </div>
     );
   }
 };
