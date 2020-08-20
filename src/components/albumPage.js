5af6f56be07ea1a6dca402b23b25fe2321fbd032
dbartleman <drewbartleman@gmail.com>
2020-08-19 21:21:17 -0700

    album page progress

--- src/components/albumPage.js
+++ src/components/albumPage.js
@@ -1,34 +1,58 @@
 import React from "react";
-import * as Vibrant from 'node-vibrant'
 import urlGenerator from "./downloader";
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: theme.spacing(3),
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    whiteSpace: 'nowrap',
+    marginBottom: theme.spacing(1),
+  },
+  divider: {
+    margin: theme.spacing(2, 0),
+  },
+}));
 
-class AlbumPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.imageRef = React.createRef();
-  }
-  componentDidMount() { // after first render
-    this.getColors(this.imageRef);
-  }
-  getColors(imageRef) {
-    Vibrant.from(`${imageRef}`).getPalette(
-      (err, palette) => { 
-        return palette; 
-      });
-    // const v = new Vibrant(urlGenerator("music/cb/cover.jpg"));
-    // console.log(v.getPalette);
-  }
-
-  render () {
-    // this.getColors(this.imageRef);
-    return (
-      <div>
-        <img src={urlGenerator("music/cb/cover.jpg")} ref={this.imageRef} alt="Album Art" />  
-        {/* {this.getColors(this.imageRef)} */}
-      </div>
-    );
-  }
+export default function AlbumPage() {
+  const classes = useStyles();
+  const divStyle = {
+    padding: '50px',
+    margin: '0 auto',
+    backgroundImage: 'urlGenerator("music/air/cover.jpg")',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  };
+  return (
+    <div style={divStyle}>
+      <Grid container direction="column" spacing={5}>
+        <Grid item xs={10}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>sample 1</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>sample 2222222222</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>sample 3</Paper>
+          </Grid>
+        </Grid>
+        
+      </Grid>
+      <Grid container direction="column" spacing={5}>
+        <Grid item xs={8} sm>
+          <img src={urlGenerator("music/air/cover.jpg")} alt='Album art - Air' />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
-
-export default AlbumPage;
