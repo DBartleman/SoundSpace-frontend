96c1e56d036f7d7f25e57f14faf8f113956aa2db
dbartleman <drewbartleman@gmail.com>
2020-08-26 22:55:54 -0700

    working on new features; buttons, hover cover flow effect, better data

--- src/components/artistPage.js
+++ src/components/artistPage.js
@@ -61,8 +61,9 @@ const useStyles = makeStyles((theme) => ({
 // development and testing only - to be replaced with state in the future
 const albumData1 = {
   artist: "The Seatbelts",
+  artistDescription: "Seatbelts is a Japanese band led by composer and instrumentalist Yoko Kanno. An international ensemble comprising both a stable lineup of musicians and various collaborators, the band was assembled by Kanno in 1998 to perform the soundtrack music for the Cowboy Bebop anime series.",
   album: "Cowboy Bebop",
-  description: "Cowboy Bebop is the first album created for the series, and the most easily categorized in terms of genre, as an outlet for many of the trademark bebop tracks. It begins with the show's theme song, \"Tank!\". The track \"Bad Dog No Biscuits\" opens with a cover of the Tom Waits composition \"Midtown\" before diverting in its interpretation.",
+  albumDescription: "Cowboy Bebop is the first album created for the series, and the most easily categorized in terms of genre, as an outlet for many of the trademark bebop tracks. It begins with the show's theme song, \"Tank!\". The track \"Bad Dog No Biscuits\" opens with a cover of the Tom Waits composition \"Midtown\" before diverting in its interpretation.",
   coverArt: urlGenerator("music/cb/cover.jpg"),
   songList: [
     {
@@ -85,23 +86,22 @@ const albumData1 = {
 }
 const albumData2 = {
   artist: "Air",
+  artistDescription: "Air are a French music duo from Versailles, consisting of Nicolas Godin and Jean-Benoît Dunckel. After making several remixes for other acts in the first half of the 1990s, the duo released their first album, Moon Safari, in 1998. The album received universal acclaim and became an international success.",
   album: "Moon Safari",
-  description: "Air's 1998 debut album Moon Safari is an influential masterpiece that not only started the international career of Jean-Benoît Dunckel and Nicolas Godin, but also found a loyal fanbase all over the world.",
+  albumDescription: "Air's 1998 debut album Moon Safari is an influential masterpiece that not only started the international career of Jean-Benoît Dunckel and Nicolas Godin, but also found a loyal fanbase all over the world.",
   coverArt: urlGenerator("music/air/cover.jpg"),
   songList: []
 }
 const albumData3 = {
   artist: "Hey Arnold!",
+  artistDescription: "James Volker Langknecht, better known as Jim Lang, is an American composer. He is known for scoring the Nickelodeon series Hey Arnold!, as well as it's feature film and television film.",
   album: "Jim Lang",
-  description: "Hey Arnold! is an American animated children's television series created by Craig Bartlett that aired on Nickelodeon from October 7, 1996, to June 8, 2004. The show centers on a fourth grader named Arnold. Episodes center on his experiences navigating urban life and helping fellow classmates and townsfolk.",
+  albumDescription: "Hey Arnold! is an American animated children's television series created by Craig Bartlett that aired on Nickelodeon from October 7, 1996, to June 8, 2004. The show centers on a fourth grader named Arnold. Episodes center on his experiences navigating urban life and helping fellow classmates and townsfolk.",
   coverArt: urlGenerator("music/jim/cover.jpg"),
   songList: []
 }
 
 const albums = [albumData1, albumData2, albumData3];
-
-const artistName = "Sample Artist";
-const artistDescription = "Seatbelts is a Japanese band led by composer and instrumentalist Yoko Kanno. An international ensemble comprising both a stable lineup of musicians and various collaborators, the band was assembled by Kanno in 1998 to perform the soundtrack music for the Cowboy Bebop anime series."
 
 export default function ArtistPage() {
   const classes = useStyles();
@@ -112,12 +112,13 @@ export default function ArtistPage() {
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
-          <Container maxWidth="sm">
+          {/* below container originally had maxWidth="sm" on it, but it wrapped text too much */}
+          <Container> 
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
               {albumData1.artist}
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              {artistDescription}
+              {albumData1.artistDescription}
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
@@ -151,7 +152,7 @@ export default function ArtistPage() {
                       {card.album}
                     </Typography>
                     <Typography>
-                      {card.description}
+                      {card.albumDescription}
                     </Typography>
                   </CardContent>
                   <CardActions>
