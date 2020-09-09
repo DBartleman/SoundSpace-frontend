f3d273fae69d1078cd56f9b2c597a734e0c837c8
dbartleman <drewbartleman@gmail.com>
2020-09-08 20:33:50 -0700

    styling and config testing

--- src/config.js
+++ src/config.js
@@ -11,3 +11,16 @@ export const access = process.env.REACT_APP_ACCESS_KEY;
 export const secret = process.env.REACT_APP_SECRET_ACCESS_KEY;
 export const bucket1 = process.env.REACT_APP_BUCKETNAME;
 export const expiration = 60*10;
+
+
+//-----------------------------------------------------
+
+// SAMPLE env file - for studying and reference
+// require("dotenv").config(); 
+// const baseUrl = process.env.BASE_URL || "http://localhost:8080/";
+// const database = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : process.env.MONGODB_URI_DEV; 
+// const port = process.env.PORT || 8080;
+// const environment = process.env.NODE_ENV || 'development';
+// const myEnv = {  ,  baseUrl,  database, port, environment};
+// module.exports = myEnv;  
+ 
